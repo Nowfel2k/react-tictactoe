be9a9e1c172c57fab8245724540c7f79a162702a
nowfel2k <muhammednavfal1@gmail.com>
2020-09-28 10:12:34 +0530

    Updates

---
+++ precache-manifest.ef130cb1929128639215083f101c9135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93dbfb38b3fd9277e2171c7e0fd68cd4",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "c785281a9c5dd7faeffe",
+    "url": "/react-tictactoe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3389eb0ec529da3c9b5",
+    "url": "/react-tictactoe/static/js/2.05fb6b8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tictactoe/static/js/2.05fb6b8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c785281a9c5dd7faeffe",
+    "url": "/react-tictactoe/static/js/main.85eed5a9.chunk.js"
+  },
+  {
+    "revision": "db82b3e8f146d9408a77",
+    "url": "/react-tictactoe/static/js/runtime-main.c287636c.js"
+  }
+]);
